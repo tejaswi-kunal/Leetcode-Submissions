1c41ee09a6bab987836cc4e942ba28e1469bdbf6
Tejaswi Kunal <tejaswi.kunal.in@gmail.com>
2026-07-11 11:45:46 +0530

    Time: 26 ms (65.11%), Space: 27.4 MB (69.22%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,9 +4,28 @@ public:
     {
         int n=s1.size(),m=s2.size();
 
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
 
-        return f(n,m,s1,s2,dp);
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                    dp[i][j]=dp[i-1][j-1]+1;
+                }
+
+                else
+                {
+                    int c1=dp[i-1][j];
+                    int c2=dp[i][j-1];
+
+                    dp[i][j]=max(c1,c2);
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 
     int f(int i,int j,string &s1,string &s2,vector<vector<int>>&dp)
