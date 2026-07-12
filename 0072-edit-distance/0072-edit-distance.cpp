1b8ff273ddd7bc48f4b02bf34b817dae02b0e0bd
Tejaswi Kunal <tejaswi.kunal.in@gmail.com>
2026-07-13 01:52:48 +0530

    Time: 6 ms (68.59%), Space: 10.4 MB (96.89%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int minDistance(string s1, string s2) 
+    {
+        int n=s1.size();
+        int m=s2.size();
+        
+        vector<int>prev(m+1,0),curr(m+1,0);
+
+        //now lets apply the tabulation approch
+        //base case
+
+        for(int j=0;j<=m;j++)
+        {
+            prev[j]=j;
+        }
+
+        for(int i=1;i<=n;i++)
+        {
+            curr[0]=i;
+            for(int j=1;j<=m;j++)
+            {
+                //now here we have two outcomes
+                if(s1[i-1]==s2[j-1])
+                {
+                    curr[j]=prev[j-1];
+                }
+
+                else
+                {
+                    curr[j]=1+min(prev[j],min(curr[j-1],prev[j-1]));
+                }
+            }
+
+            prev=curr;
+        }
+
+
+        return  prev[m];
+    }
+};
