1f94758bfff1386646447b2440849d2982396d6b
Tejaswi Kunal <tejaswi.kunal.in@gmail.com>
2026-07-15 19:16:18 +0530

    Time: 7 ms (99.57%), Space: 14 MB (26.37%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -5,12 +5,34 @@ public:
         cuts.push_back(0);
         cuts.push_back(n);
 
+        
+
         sort(cuts.begin(),cuts.end());
 
-        vector<vector<int>>dp(cuts.size(),vector<int>(cuts.size(),-1));
+        vector<vector<int>>dp(cuts.size(),vector<int>(cuts.size(),0));
+        int c=cuts.size();
+
+        // now lets try the tabulation
+        for(int i=c-2;i>=1;i--)
+        {
+            for(int j=i;j<=c-2;j++)
+            {
+                int cost=0,mini=INT_MAX;
+
+                for(int k=i;k<=j;k++)
+                {
+                    cost=(cuts[j+1]-cuts[i-1])+dp[i][k-1]+dp[k+1][j];
+                    mini=min(cost,mini);
+                }
+
+                dp[i][j]=mini;
+            }
+        }
+
+        return dp[1][c-2];
 
         // this function will return what is the minimum cost to apply the cuts from index 1---->cuts.size()-2
-        return f(1,cuts.size()-2,cuts,dp);
+        // return f(1,cuts.size()-2,cuts,dp);
     }
 
     int f(int i,int j,vector<int>&cuts,vector<vector<int>>&dp)
