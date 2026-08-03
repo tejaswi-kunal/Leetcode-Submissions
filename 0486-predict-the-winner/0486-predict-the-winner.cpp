85e96cbeb89a07a7df001dede914cf2fae50bf00
Tejaswi Kunal <tejaswi.kunal.in@gmail.com>
2026-08-03 20:29:31 +0530

    Time: 0 ms (100%), Space: 10.7 MB (5.78%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -2,47 +2,39 @@ class Solution {
 public:
     bool predictTheWinner(vector<int>& nums) 
     {
-        // first rec approch --> rec wrt to p1 --->in this player1 will want to maximize his score and p2 will try to 
-        // minimize the score of p1 
+        // now instead of calculating best score of player1 ,we can directly calculate the diffrence of ply1-ply2
         int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
+        vector<vector<int>>dp(n,vector<int>(n));
+        vector<vector<bool>>visited(n,vector<bool>(n,0));
 
-        int player1=solve(0,n-1,dp,nums);
-
-        int player2=accumulate(nums.begin(),nums.end(),0)-player1;
-
-        if(player1>=player2)
+        if(solve(0,n-1,nums,dp,visited)>=0)
         {
             return 1;
         }
 
         return 0;
-
-        
+    
     }
 
-    int solve(int i,int j,vector<vector<int>>&dp,vector<int>&nums)
+    int solve(int i,int j,vector<int>&nums,vector<vector<int>>&dp,vector<vector<bool>>&visited)
     {
         if(i>j)
         {
             return 0;
         }
 
-        if(i==j)
-        {
-            return nums[i];
-        }
-
-        if(dp[i][j]!=-1)
+        if(visited[i][j])
         {
             return dp[i][j];
         }
 
-        // now we have two choices for p1
+        // now we have two choices each time for each player
         int c1=0,c2=0;
 
-        c1=nums[i]+min(solve(i+2,j,dp,nums),solve(i+1,j-1,dp,nums));
-        c2=nums[j]+min(solve(i+1,j-1,dp,nums),solve(i,j-2,dp,nums));
+        c1=nums[i]-solve(i+1,j,nums,dp,visited);
+        c2=nums[j]-solve(i,j-1,nums,dp,visited);
+
+        visited[i][j]=1;
 
         return dp[i][j] = max(c1,c2);
     }
