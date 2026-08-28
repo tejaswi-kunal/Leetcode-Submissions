224ce8499b9638d0c85c227fc8446c39563a5f48
Tejaswi Kunal <tejaswi.kunal.in@gmail.com>
2026-08-28 16:15:36 +0530

    Time: 0 ms (100%), Space: 10.1 MB (69.64%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
@@ -1,64 +1,75 @@
 class Solution {
 public:
-    string lexGreaterPermutation(string s, string target) {
-        int n = s.size();
+    string lexGreaterPermutation(string s, string target) 
+    {
+        int n=s.size();
+        string ans;
 
-        // Frequency of characters in s
-        vector<int> cnt(26, 0);
+        // first we have to count the freq of each character 
+        vector<int>mp(26,0);
 
-        for (char c : s) {
-            cnt[c - 'a']++;
+        for(int i=0;i<n;i++)
+        {
+            mp[s[i]-'a']++;
         }
 
-        // Try the position where we make the string greater.
-        // Rightmost position is preferred.
-        for (int i = n - 1; i >= 0; i--) {
+        // now we have to construct the ans
+        string temp;
+        solve(target,ans,temp,mp,false,0);
 
-            // Rebuild the frequency array for this pivot.
-            vector<int> remain = cnt;
+        return ans;
+        
+    }
 
-            // Try to keep target[0 ... i-1] unchanged.
-            bool possible = true;
+    void solve(string &t,string &ans,string &temp,vector<int>&mp,bool found,int i)
+    {
 
-            for (int j = 0; j < i; j++) {
-                int x = target[j] - 'a';
-
-                if (remain[x] == 0) {
-                    possible = false;
-                    break;
+        if(found)
+        {
+            // now we have to fill the rest of the letters in the sorted manner 
+            for(int j=0;j<26;j++)
+            {
+                while(mp[j])
+                {
+                    temp.push_back(j+'a');
+                    mp[j]--;
                 }
-
-                remain[x]--;
             }
-
-            if (!possible)
-                continue;
-
-            // At position i, we need the smallest
-            // available character strictly greater than target[i].
-            int targetChar = target[i] - 'a';
-
-            for (int c = targetChar + 1; c < 26; c++) {
-
-                if (remain[c] == 0)
-                    continue;
-
-                string ans = target.substr(0, i);
-
-                // Make the first difference here.
-                ans += char('a' + c);
-
-                remain[c]--;
-
-                // Fill the rest in sorted order.
-                for (int x = 0; x < 26; x++) {
-                    ans.append(remain[x], char('a' + x));
-                }
-
-                return ans;
-            }
+            ans=temp;
+            return;
         }
 
-        return "";
+        if(i==t.size())
+        {
+            if(found)
+            {
+                ans=temp;
+            }
+            return;
+        }
+
+        for(int j=t[i]-'a';j<26;j++)
+        {
+            if(mp[j])
+            {
+                temp.push_back(j+'a');
+                mp[j]--;
+
+                if(t[i]<j+'a')
+                {
+                    found=1;
+                }
+
+                solve(t,ans,temp,mp,found,i+1);
+
+                temp.pop_back();
+                mp[j]++;
+            }
+            // we dont have to explore further
+            if(ans.size()!=0)
+            {
+                return;
+            }
+        }
     }
 };
