7a22a9f43ad0badac21c677cc4f60f3e00bcbf54
Tejaswi Kunal <tejaswi.kunal.in@gmail.com>
2026-08-31 21:30:54 +0530

    Time: 236 ms (10%), Space: 65.2 MB (60%) - LeetHub

--- 4040-minimum-operations-to-form-subset-sum-i/4040-minimum-operations-to-form-subset-sum-i.cpp
+++ 4040-minimum-operations-to-form-subset-sum-i/4040-minimum-operations-to-form-subset-sum-i.cpp
@@ -1,62 +1,74 @@
 class Solution {
 public:
-    int dp[101][5005];
     int minOperations(vector<int>& nums, int sum) 
     {
-        memset(dp,-1,sizeof(dp));
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(sum+1,0));
 
-        int ans = solve(nums.size()-1,sum,nums);
-
-        if(ans<1e8)
+        // first we have to define the base case
+        for(int i=1;i<=sum;i++)
         {
-            return ans;
+            dp[0][i]=1e8;
+        }
+
+        int f=nums[0];
+        int c=0;
+        while(f<=sum)
+        {
+            dp[0][f]=c;
+            c++;
+            f*=2;
+        }
+
+        f=nums[0]/2;
+        c=1;
+        while(f>0)
+        {
+            if(f<=sum)
+            dp[0][f]=c;
+            
+            f/=2;
+            c++;
+        }
+
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<=sum;j++)
+            {
+                int c1=1e8,c2=1e8,c3=1e8;
+                int curr=nums[i];
+                int count=0;
+                while(curr<=j)
+                {
+                    c1=min(c1,dp[i-1][j-curr]+count);
+                    count++;
+                    curr*=2;
+                }
+
+                curr=nums[i]/2;
+                count=1;
+                while(curr>0)
+                {
+                    if(j-curr>=0)
+                    {
+                        c2=min(c2,dp[i-1][j-curr]+count);
+                    }
+                    curr/=2;
+                    count++;
+                }
+
+                c3=dp[i-1][j];
+
+                dp[i][j]=min({c1,c2,c3});
+            }
+        }
+
+        if(dp[n-1][sum]<1e8)
+        {
+            return dp[n-1][sum];
         }
 
         return -1;
-    }
-
-    int solve(int i,int sum,vector<int>&nums)
-    {
-        if(!sum)
-        {
-            return 0;
-        }
-
-        if(i<0)
-        {
-            return 1e8;
-        }
-
-        if(dp[i][sum]!=-1)
-        {
-            return dp[i][sum];
-        }
-
-        int c2=1e8,c3=1e8,c4=1e8;
-        int curr=nums[i];
-
-        // 2-->take the multiple of nums[i]
-        int count=0;
-        while(curr<=sum)
-        {
-            c2=min(c2,solve(i-1,sum-curr,nums)+count);
-            count++;
-            curr*=2;
-        }
-
-        count=1;
-        curr=nums[i]/2;
-        while(curr>0)
-        {
-            if(sum-curr>=0)
-            c3=min(c3,solve(i-1,sum-curr,nums)+count);
-            
-            count++;
-            curr/=2;
-        }
-
-        c4=solve(i-1,sum,nums);
-
-        return dp[i][sum] = min({c2,c3,c4});
+        
     }
 };
